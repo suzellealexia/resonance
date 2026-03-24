e601c0df1de3d93c5dccb896cdfa94c736df8e97
suzellealexia <engaalexiasuzelle@gmail.com>
2026-03-24 01:57:26 +0100

    01: project setup, authentication & database

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center gap-4 bg-background">
+      <h1 className="text-2xl font-semibold">Welcome to Resonance</h1>
+      <div className="flex items-center gap-4">
+        <OrganizationSwitcher />
+        <UserButton />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
